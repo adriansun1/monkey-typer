f0d5a4138de003254bed7ea44858b0aa8b7337d6
Adrian <34041016+Sgshi@users.noreply.github.com>
2018-12-28 16:25:36 -0800

    bugged falling banana

--- src/client/components/Banana.jsx
+++ src/client/components/Banana.jsx
@@ -9,6 +9,7 @@ const Banana = props => {
   
   return(
     <div id="banana-wrap">
+      <FallingBananaHome />
       <h1>{Math.floor(props.totalBananas)}</h1>
       <img id="banana" src={require("../img/banana.png")} alt="dis a banana" />
     </div>
